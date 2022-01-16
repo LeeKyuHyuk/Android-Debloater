1a7249c041bf5e39c31f18a866f57e58b5a434b4
KyuHyuk Lee <lee@kyuhyuk.kr>
2022-01-16 18:28:38 +0900

    fix: ADB cannot execute if there is a space in the install path

--- src/adb/Adb.ts
+++ src/adb/Adb.ts
@@ -2,9 +2,11 @@ import { join } from 'path';
 import { app } from '@electron/remote';
 
 const Adb = () => {
-  if (process.platform === 'linux') return join(app.getAppPath() + '.unpacked', '/adb');
-  if (process.platform === 'darwin') return join(app.getAppPath() + '.unpacked', '/adb');
-  return join(app.getAppPath() + '.unpacked', '\\adb.exe');
+  if (process.platform === 'linux')
+    return join(app.getAppPath() + '.unpacked', '/adb').replace(/ /g, '\\ ');
+  if (process.platform === 'darwin')
+    return join(app.getAppPath() + '.unpacked', '/adb').replace(/ /g, '\\ ');
+  return join(app.getAppPath() + '.unpacked', '\\adb.exe').replace(/ /g, '\\ ');
 };
 
 export default Adb;
